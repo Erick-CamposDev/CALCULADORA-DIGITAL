cee98c736974a7e2e82dd9c8e0512ba90925dc0f
Erick Fellipe <efrc22br@gmail.com>
2025-09-18 14:59:28 -0300

    Feat: Adicionado interatividade a calculadora

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,33 +3,65 @@ const display = document.getElementById("display");
 let currentValue = "";
 
 function appendNumber(value) {
-  display.innerHTML += `<p>${value}</p>`;
+  display.innerText += `${value}`;
   currentValue += value;
 }
 
 function appendOperator(operator) {
-  if (currentValue === "" && operator !== "-") {
+  if (currentValue === "" && operator === "-") {
+    display.innerText += `${operator}`;
+    currentValue += operator;
+  }
+
+  if (currentValue === "") {
     return;
   }
+
   const lastValue = currentValue.slice(-1);
 
   if ("+-÷x,".includes(lastValue)) {
     return;
   }
-  display.innerHTML += `<p>${operator}</p>`;
+  display.innerText += `${operator}`;
   currentValue += operator;
 }
 
-function removeLastValue() {
-  const values = display.querySelectorAll("p");
-  if (values.length > 0) {
-    let lastNumber = values.length - 1;
-    values[lastNumber].remove();
-    currentValue = currentValue.slice(0, -1);
+function appendComma(comma) {
+  if (currentValue === "" && comma === ",") {
+    return;
   }
+
+  const lastValue = currentValue.slice(-1);
+
+  if (comma === "," && lastValue === ",") {
+    return;
+  }
+
+  if (comma === "," && currentValue.includes(",")) {
+    return;
+  }
+
+  display.innerText += `${comma}`;
+  currentValue += comma;
 }
 
-function calculateResult() {}
+function removeLastValue() {
+  currentValue = currentValue.slice(0, -1);
+  display.innerText = currentValue;
+}
+
+function calculateResult() {
+  try {
+    let expression = currentValue.replace(",", ".");
+
+    const result = eval(expression);
+    currentValue = String(result);
+    display.innerText = currentValue.replace(/\./g, ",");
+  } catch {
+    alert("ERRO!!!");
+    display.innerText = "ERRO!!!";
+  }
+}
 
 function cleanDisplay() {
   display.innerHTML = "";
